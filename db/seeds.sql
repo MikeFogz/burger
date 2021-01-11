a0a105d2361ea19109edb7db5d0badd74c794396
MikeFogz <MikeFogz@gmail.com>
2021-01-10 22:42:53 -0800

    Added schema & seeds .sql

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Double Double');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Burger');
+INSERT INTO burgers (burger_name) VALUES ('Big Mac');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Rory', true);
