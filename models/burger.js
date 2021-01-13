5627e23c80ff54d266fc96cdea7dce69dd7eeb3f
MikeFogz <MikeFogz@gmail.com>
2021-01-13 03:34:31 -0800

    finished models with updateOne for devouring

--- models/burger.js
+++ models/burger.js
@@ -1,13 +1,18 @@
-const orm = require('../config/orm.js');
+const orm = require('../config/orm');
 
 const burger = {
+    //select all
     selectAll(burgerCallback) {
         orm.selectAll("burgers", (res) => burgerCallback(res));
     },
+    //add new burger
     insertOne(cols, vals, burgerCallback) {
         orm.insertOne("burgers", cols, vals, (res) => burgerCallback(res));
     },
-    //devour
+    //devour a burger
+    updateOne(objColVals, condition, burgerCallback) {
+        orm.updateOne("burgers", objColVals, condition, (res) => burgerCallback(res));
+    },
 };
 
 module.exports = burger;
