8cd26e24797e0b53d795c4c56390b9b617936888
MikeFogz <MikeFogz@gmail.com>
2021-01-14 01:19:48 -0800

    Added routes to insert new burgers into table and update the db.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,14 +4,42 @@ const router = express.Router();
 
 const burger = require('../models/burger.js');
 
+//start routes
 router.get('/', (req, res) => {
     burger.selectAll((data) => {
-      const hbsObject = {
+    const hbsObject = {
         burgers: data,
-      };
-      console.log(hbsObject);
-      res.render('index', hbsObject);
+    };
+    console.log(hbsObject);
+    res.render('index', hbsObject);
     });
-  });
+});
+
+//insert new burger into table
+router.post("/api/burgers", (req, res) => {
+    burger.insertOne([req.body.burger_name], (result) => {
+    res.json({ id: result.insertId });
+    });
+});
+
+  //update the burger database
+router.put("/api/burgers/:id", (req, res) => {
+    const condition = `id = ${req.params.id}`;
+
+    console.log("condition", condition);
+
+    burger.updateOne(
+    {
+        devoured: true,
+    },
+    condition,
+    (result) => {
+        if (result.changedRows === 0) {
+        return res.status(404).end();
+        }
+        res.status(200).end();
+    }
+    );
+});
 
 module.exports = router;
