378dbb4adf26f2c240eeb8dc5a5217d8fdd03269
MikeFogz <MikeFogz@gmail.com>
2021-01-14 17:49:52 -0800

    slight tweaks to server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
-const mysql = require('mysql');
 
 var PORT = process.env.PORT || 8080;
 
@@ -14,7 +13,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Set Handlebars.
-// var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
@@ -27,5 +25,5 @@ app.use(routes);
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
   // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+console.log("Server listening on: http://localhost:" + PORT);
 });
